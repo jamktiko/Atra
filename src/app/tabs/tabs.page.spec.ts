f81209dd0ec0427ab9e889de40394addb4e7b0ea
Juuso Jääskeläinen <AF0313@student.jamk.fi>
2025-09-30 11:10:02 +0300

    added unit tests to tabs & user

--- src/app/tabs/tabs.page.spec.ts
+++ src/app/tabs/tabs.page.spec.ts
@@ -20,7 +20,32 @@ describe('TabsPage', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  // Test to check if the correct number of ion-buttons are rendered
+  it('should render the 5 buttons', () => {
+    const compiled = fixture.nativeElement as HTMLElement;
+    const tabBar = compiled.querySelector('ion-tab-bar');
+    expect(tabBar).toBeTruthy();
+    console.log(tabBar);
+
+    // CHANGE WHEN ALTERING TAB-BUTTONS
+    const buttons = tabBar?.querySelectorAll('ion-tab-button');
+    console.log(buttons);
+    expect(buttons?.length).toBe(5);
+  });
+
+  // Test to check if first button in tab-bar is mainpage
+  it('should have the first button as mainpage', () => {
+    const compiled = fixture.nativeElement as HTMLElement;
+    const tabBar = compiled.querySelector('ion-tab-bar');
+    // CHANGE WHEN ALTERING TAB-BUTTONS
+    const buttons = tabBar?.querySelectorAll('ion-tab-button');
+
+    const firstButton = buttons?.[0];
+    expect(firstButton).toBeTruthy();
+    if (firstButton) {
+      expect(firstButton.getAttribute('tab')).toBe('mainpage');
+    } else {
+      fail('First button not found');
+    }
   });
 });
--- src/app/user/user.page.spec.ts
+++ src/app/user/user.page.spec.ts
@@ -2,7 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { UserPage } from './user.page';
 
 describe('UserPage', () => {
-  /*   let component: UserPage;
+  let component: UserPage;
   let fixture: ComponentFixture<UserPage>;
 
   beforeEach(() => {
@@ -13,8 +13,8 @@ describe('UserPage', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
-  }); */
-  it('should pass', () => {
-    expect(true).toBe(true);
   });
+  /*   it('should pass', () => {
+    expect(true).toBe(true);
+  }); */
 });
