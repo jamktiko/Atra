b7463153715ed640313516216ee7597f2803d614
Juuso Jääskeläinen <AF0313@student.jamk.fi>
2025-09-30 13:03:44 +0300

    working test on publiclibrary component

--- src/app/inks/components/publiclibrary/publiclibrary.page.spec.ts
+++ src/app/inks/components/publiclibrary/publiclibrary.page.spec.ts
@@ -1,9 +1,10 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+// auto-generated: does not work
+/* import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { PubliclibraryPage } from './publiclibrary.page';
 
 describe('PubliclibraryPage', () => {
   //this does not pass
-  /*   let component: PubliclibraryPage;
+    let component: PubliclibraryPage;
   let fixture: ComponentFixture<PubliclibraryPage>;
 
   beforeEach(() => {
@@ -14,10 +15,37 @@ describe('PubliclibraryPage', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
-  }); */
+  });
 
   //this passes
   it('should pass', () => {
     expect(true).toBe(true);
+  });
+});
+ */
+
+// this works
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { provideRouter } from '@angular/router';
+import { PubliclibraryPage } from './publiclibrary.page';
+
+describe('PubliclibraryPage', () => {
+  let component: PubliclibraryPage;
+  let fixture: ComponentFixture<PubliclibraryPage>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [PubliclibraryPage, HttpClientTestingModule],
+      providers: [provideRouter([])],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(PubliclibraryPage);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
   });
 });
