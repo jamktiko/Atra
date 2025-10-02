fcb1663a17345026d270b75d8507c7b5b467939c
Lotta Näsman <ae3398@student.jamk.fi>
2025-10-02 12:48:37 +0300

    Added Lambda function for seeding data to db

--- backend/functions/shared/db.ts
+++ backend/functions/shared/db.ts
@@ -20,7 +20,7 @@ export async function getPool() {
     database: dbSecret.dbname,
     password: dbSecret.password,
     port: dbSecret.port,
-    ssl: { rejectUnauthorized: true },
+    ssl: { rejectUnauthorized: false },
   });
   return pool;
 }
