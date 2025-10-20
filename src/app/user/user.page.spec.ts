43b8441ee5e17de7ce66732f699901497025061d
Juuso Jääskeläinen <AF0313@student.jamk.fi>
2025-10-20 14:11:51 +0300

    userpage unit tests

--- src/app/user/user.page.spec.ts
+++ src/app/user/user.page.spec.ts
@@ -14,4 +14,15 @@ describe('UserPage', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should have h2 title', () => {
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('h2')?.textContent).toContain('Hi');
+  });
+
+  it('should have 5 buttons', () => {
+    const compiled = fixture.nativeElement as HTMLElement;
+    const buttons = compiled.querySelectorAll('button');
+    expect(buttons.length).toBe(5);
+  });
 });
