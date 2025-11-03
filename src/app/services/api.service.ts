840458a6cc10623f608e1df4cd6d6989c1aca2d7
Sally <sally.liponen@domain.fi>
2025-11-03 10:01:41 +0200

    Debugging

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -27,10 +27,10 @@ import { is } from 'cypress/types/bluebird';
 export class ApiService {
   private apiUrl = environment.apiUrl;
   //TÄMÄ KUN DEV
-  // private readonly isProd = environment.production;
+  private readonly isProd = environment.production;
   //false when using ionic serve, true when using ionic build
   //TÄMÄ KUN PROD
-  private readonly isProd = true; //this is for testing: fakes that we are in prod branch after ionic build
+  // private readonly isProd = true; //this is for testing: fakes that we are in prod branch after ionic build
 
   private localUserInks: UserInk[] = [...mockUserInks]; //copy of mockUserInks
   private localCustomers: Customer[] = [...mockCustomers]; //copy of mockCustomers
