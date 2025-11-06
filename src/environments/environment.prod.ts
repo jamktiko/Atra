2a828dde70b9b41d0f15434a8f4fdaa949fe01e2
Juuso Jääskeläinen <AF0313@student.jamk.fi>
2025-11-06 12:33:57 +0200

    e2e tests for entries

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   gitBranch: 'prod',
-  apiUrl: 'https://ziwxm2u2s0.execute-api.eu-north-1.amazonaws.com', // will be injected during CI/CD
+  apiUrl: 'https://lccr0w6zj3.execute-api.eu-north-1.amazonaws.com', // will be injected during CI/CD
 };
