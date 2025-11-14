7a445a203f7d0e176753c140c30fcaad6ff1ae9a
Juuso Jääskeläinen <AF0313@student.jamk.fi>
2025-11-14 12:37:39 +0200

    cognito stack test fixed for real

--- backend/test/cognito.test.ts
+++ backend/test/cognito.test.ts
@@ -1,15 +1,42 @@
-import { App } from 'aws-cdk-lib';
+import { App, Stack } from 'aws-cdk-lib';
 import { CognitoStack } from '../lib/cognito-stack';
 import { Match, Template } from 'aws-cdk-lib/assertions';
-
-const app = new App();
-const stack = new CognitoStack(app, 'TestStack', {
-  vpc: {} as any, // tai mockattu VPC
-  rdsSecretName: 'dummy-secret',
-});
-const template = Template.fromStack(stack);
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
 
 describe('CognitoStack', () => {
+  let app: App;
+  let template: Template;
+
+  beforeEach(() => {
+    app = new App();
+
+    // Luo erillinen VPC-stack
+    const vpcStack = new Stack(app, 'TestVpcStack');
+    const vpc = new ec2.Vpc(vpcStack, 'TestVpc', {
+      maxAzs: 1,
+      subnetConfiguration: [
+        { name: 'Public', subnetType: ec2.SubnetType.PUBLIC, cidrMask: 24 },
+        {
+          name: 'Private',
+          subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS,
+          cidrMask: 24,
+        },
+        {
+          name: 'Isolated',
+          subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
+          cidrMask: 24,
+        },
+      ],
+    });
+
+    const stack = new CognitoStack(app, 'TestStack', {
+      vpc,
+      rdsSecretName: 'dummy-secret',
+    });
+
+    template = Template.fromStack(stack);
+  });
+
   test('Creates a Cognito User Pool Client with OAuth settings', () => {
     template.hasResourceProperties('AWS::Cognito::UserPoolClient', {
       ClientName: 'AtraAppClient',
@@ -18,16 +45,12 @@ describe('CognitoStack', () => {
         'ALLOW_USER_SRP_AUTH',
       ]),
       SupportedIdentityProviders: ['COGNITO'],
-      CallbackURLs: Match.anyValue(), //checks only that the property exists
-      LogoutURLs: Match.anyValue(), //checks only that the property exists
+      CallbackURLs: Match.anyValue(),
+      LogoutURLs: Match.anyValue(),
     });
   });
 
   test('Creates SSM parameters for User Pool ID and Client ID', () => {
     template.resourceCountIs('AWS::SSM::Parameter', 2);
   });
-
-  // TODO: implement test if users would be created in the user pool automatically
-  /*   test('Creates a User in the User Pool', () => {
-  }) */
 });
