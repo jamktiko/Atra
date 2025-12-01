90a6f9cbfac2deda266b5c2dad44feb8c3eb5447
Juuso Jääskeläinen <AF0313@student.jamk.fi>
2025-12-01 14:27:07 +0200

    e2e tests, not working but newest version

--- cypress/plugins/index.ts
+++ cypress/plugins/index.ts
@@ -1,29 +1,20 @@
-const {
+import {
   CognitoIdentityProviderClient,
   InitiateAuthCommand,
-} = require('@aws-sdk/client-cognito-identity-provider');
+} from '@aws-sdk/client-cognito-identity-provider';
 
 interface LoginCognitoCredentials {
   username: string;
   password: string;
 }
 
-type TaskHandler = (...args: any[]) => any;
-
-interface PluginOn {
-  (event: 'task', tasks: Record<string, TaskHandler>): void;
-}
-
-interface PluginConfig {
-  [key: string]: any;
-}
-
-module.exports = (on: PluginOn, config: PluginConfig) => {
+export default (on: any, config: any) => {
   on('task', {
     async loginCognito({ username, password }: LoginCognitoCredentials) {
       const client = new CognitoIdentityProviderClient({
         region: 'eu-north-1',
       });
+
       const command = new InitiateAuthCommand({
         AuthFlow: 'USER_PASSWORD_AUTH',
         ClientId: Cypress.env('COGNITO_CLIENT_ID') as string,
@@ -32,8 +23,14 @@ module.exports = (on: PluginOn, config: PluginConfig) => {
           PASSWORD: password,
         },
       });
+
       const response = await client.send(command);
-      return response.AuthenticationResult; // ✅ Return IdToken, AccessToken, RefreshToken
+
+      if (!response.AuthenticationResult) {
+        throw new Error('Cognito login failed: ' + JSON.stringify(response));
+      }
+
+      return response.AuthenticationResult; // { IdToken, AccessToken, RefreshToken }
     },
   });
 };
